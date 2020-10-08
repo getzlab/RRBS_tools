5126eaf7a479bebe3e2ec36d937ade9db1ebb3c0
Andrew Dunford <adunford@broadinstitute.org>
2020-10-08 17:32:54 -0400

    adding script to extract read stats from bismark report and updating docker to install beautifulsoup

---
+++ Bismark/extract_bismark_reads_reported.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+
+#br_file = sys.argv[0]
+br_file = 'toy500K_bismark_report.html'
+
+br = BeautifulSoup(open(br_file),'html.parser')
+sequence_table = br.body.table.find_all('td')
+string_table = br.body.table.find_all('th')
+for i in range(0,len(sequence_table)) :
+    print('%s : %s' % (string_table[i].string, sequence_table[i].string))
+    
+total_reads = sequence_table[0].string
+unique_reads = sequence_table[1].string
+unaligned_reads = sequence_table[2].string
+non_unique_reads = sequence_table[3].string
+non_extractable_reads = sequence_table[4].string
