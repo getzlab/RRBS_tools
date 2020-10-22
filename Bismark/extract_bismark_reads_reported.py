52ca622858bea6c7c5f21be464b3d5b4c05b741d
Andrew Dunford <adunford@broadinstitute.org>
2020-10-22 13:21:34 -0400

    updating script to parse bismark report to match equivalent report for bsmap

--- Bismark/extract_bismark_reads_reported.py
+++ Bismark/extract_bismark_reads_reported.py
@@ -1,8 +1,10 @@
-from bs4 import BeautifulSoup
+import pandas as pd
 import sys
+from bs4 import BeautifulSoup
 
 br_file = sys.argv[1]
-#br_file = 'toy500K_bismark_report.html'
+paired_string = sys.argv[2]
+
 
 br = BeautifulSoup(open(br_file),'html.parser')
 sequence_table = br.body.table.find_all('td')
@@ -10,8 +12,23 @@ string_table = br.body.table.find_all('th')
 for i in range(0,len(sequence_table)) :
     print('%s : %s' % (string_table[i].string, sequence_table[i].string))
     
-total_reads = sequence_table[0].string
-unique_reads = sequence_table[1].string
-unaligned_reads = sequence_table[2].string
-non_unique_reads = sequence_table[3].string
-non_extractable_reads = sequence_table[4].string
+total_reads = int(sequence_table[0].string)
+unique_reads = int(sequence_table[1].string)
+unaligned_reads = int(sequence_table[2].string)
+non_unique_reads = int(sequence_table[3].string)
+non_extractable_reads = int(sequence_table[4].string)
+
+
+
+sample_id = str.replace(br_file,'_bismark_report.html','')
+report = pd.DataFrame({'sample_id' : sample_id,
+'paired_str' : paired_string,
+'total_reads' : total_reads,
+'aligned_reads': unique_reads + non_unique_reads,
+'aligned_reads_pc' : (unique_reads + non_unique_reads)/total_reads,
+'mapped_unique' : unique_reads,
+'mapped_unique_pc' : unique_reads/total_reads,
+'mapped_non_unique' : non_unique_reads,
+'mapped_non_unique_pc' : non_unique_reads/total_reads },index=[0])
+
+report.to_csv('%s_bismark_bamstats.tsv' % sample_id,sep='\t',index=False)
